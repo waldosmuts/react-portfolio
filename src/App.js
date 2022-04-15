bea530aef84d2249a18b16f042fb16da8a712cb4
Waldo Smuts <wb.smuts@gmail.com>
2022-04-15 12:58:47 +0200

    removed seed helper

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./routes/Home";
 import Projects from "./routes/Projects";
 import Error from "./routes/Error";
 import { useState } from "react";
-import Seed from "./seed";
+// import Seed from "./seed";
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(localStorage.getItem("darkMode") || false)
@@ -21,7 +21,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home darkMode={darkMode} changeTheme={changeTheme} />} />
         <Route path="projects" element={<Projects darkMode={darkMode} changeTheme={changeTheme} />} />
-        <Route path="seed" element={<Seed />} />
+        {/* <Route path="seed" element={<Seed />} /> */}
         <Route path="*" element={<Error darkMode={darkMode} changeTheme={changeTheme} />} />
       </Routes>
     </>
